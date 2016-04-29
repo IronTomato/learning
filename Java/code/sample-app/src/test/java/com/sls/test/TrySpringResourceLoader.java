72f8dfbc0907fe140ca19130d1ebf044cadcbc67
IronTomato <weinaibin@qq.com>
2016-04-29 15:34:17 +0800

    debug Spring ApplicationContext load

--- Java/code/sample-app/src/test/java/com/sls/test/TrySpringResourceLoader.java
+++ Java/code/sample-app/src/test/java/com/sls/test/TrySpringResourceLoader.java
@@ -5,14 +5,18 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import com.sls.sample.dao.CityDao;
+
 public class TrySpringResourceLoader {
 	
 
-	@Test
+//	@Test
 	public void test() throws IOException {
 		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 		Resource[] resources = resolver.getResources("classpath*:**/*.xml");
@@ -20,6 +24,12 @@ public class TrySpringResourceLoader {
 			System.out.println(resource.getFilename());
 		}
 //		ClassPathXmlApplicationContext
+	}
+	
+	@Test
+	public void testSpringStart(){
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ctx.getBean(CityDao.class);
 	}
 
 }
