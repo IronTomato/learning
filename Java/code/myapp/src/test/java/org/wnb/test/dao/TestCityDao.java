a3483f942628803b53e09f71e5f5e73216c942fb
IronTomato <weinaibin@qq.com>
2016-05-03 20:40:37 +0800

    spring test transaction rollback

--- Java/code/myapp/src/test/java/org/wnb/test/dao/TestCityDao.java
+++ Java/code/myapp/src/test/java/org/wnb/test/dao/TestCityDao.java
@@ -6,24 +6,25 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import org.wnb.dao.CityDao;
 import org.wnb.dao.impl.CityDaoImpl;
 import org.wnb.entity.City;
 
-@ContextConfiguration(locations={"classpath:applicationContext.xml"})
-public class TestCityDao extends AbstractJUnit4SpringContextTests{
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@TransactionConfiguration(defaultRollback = true)
+public class TestCityDao extends AbstractJUnit4SpringContextTests {
 
     @Autowired
     private CityDao dao;
 
-     @Test
+    @Test
     public void testFindById() {
         City city = dao.findById(1);
         assertEquals(city.getId().intValue(), 1);
         System.out.println(city);
 
     }
-
-
 
 }
